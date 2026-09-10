f91fd9f6c4fe04897464b65ca9047b4f2736fb22
Martin Uribe <clamytoe@gmail.com>
2026-09-10 09:07:05 -0500

    Updated version number

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -8,9 +8,4 @@ from vaults import __author__, __email__, __version__
 def test_project_settings():
     assert __author__ == "Martin Uribe"
     assert __email__ == "clamytoe@gmail.com"
-    assert __version__ == "0.1.0"
-
-
-def test_env():
-    load_dotenv()
-    assert environ.get("TEST_VALUE") == "clamytoe"
+    assert __version__ == "0.1.1"
--- vaults/__init__.py
+++ vaults/__init__.py
@@ -2,4 +2,4 @@ from vaults import *
 
 __author__ = "Martin Uribe"
 __email__ = "clamytoe@gmail.com"
-__version__ = "0.1.0"
+__version__ = "0.1.1"
